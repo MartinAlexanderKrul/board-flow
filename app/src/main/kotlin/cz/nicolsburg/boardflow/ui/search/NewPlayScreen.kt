41da32320502a6308a4e117a1eda206f3fef143e
Martin Alexander Krul <m.alexander.krul@gmail.com>
2026-05-14 14:43:33 +0200

    Update NewPlayScreen.kt

--- app/src/main/kotlin/cz/nicolsburg/boardflow/ui/search/NewPlayScreen.kt
+++ app/src/main/kotlin/cz/nicolsburg/boardflow/ui/search/NewPlayScreen.kt
@@ -1,4 +1,4 @@
-package cz.nicolsburg.boardflow.ui.search
+﻿package cz.nicolsburg.boardflow.ui.search
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.Spring
@@ -70,7 +70,7 @@ fun NewPlayScreen(
     LaunchedEffect(Unit) { viewModel.loadRecentGames() }
 
     LaunchedEffect(query) {
-        delay(600)
+        delay(800)
         viewModel.filterGames(query)
     }
 
